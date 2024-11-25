a78d47c38b31c5b7480e69c56a6256280b88d0cd
balajidharma <balajidharma@hotmail.com>
2024-11-24 21:48:03 -0500

    Added comment listener

--- src/Listeners/UpdateThreadStatistics.php
+++ src/Listeners/UpdateThreadStatistics.php
@@ -8,15 +8,9 @@ use Illuminate\Support\Facades\Config;
 
 class UpdateThreadStatistics
 {
-    /**
-     * Handle the event.
-     *
-     * @param  object  $event
-     * @return void
-     */
     public function handle($event)
     {
-        if ($event->comment->commentable_type == Config::get('forum.models.thread', Thread::class)) {
+        if ($event->comment->commentable && $event->comment->commentable instanceof (Config::get('forum.models.thread', Thread::class))) {
             $event->comment->commentable->updateStatistics();
         }
     }
